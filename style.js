45774017662c7ea4ba069787fdd02d2906c860ef
Hasanhimu005 <hasanhimu005@gmail.com>
2025-03-22 15:30:56 +0600

    finish commit

--- style.js
+++ style.js
@@ -1,3 +1,33 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("yourFormId").onsubmit = function(event) {
+        event.preventDefault();
+        connect();
+    };
+});
+
+function connect() {
+    var email = document.getElementById("email").value;
+    var firstname = document.getElementById("fname").value;
+    var lastname = document.getElementById("lname").value;
+    var phone = document.getElementById("phone").value;
+    var message = document.getElementById("message").value;
+
+    // store the values in the local storage
+    localStorage.setItem("email", email);
+    localStorage.setItem("firstname", firstname);
+    localStorage.setItem("lastname", lastname);
+    localStorage.setItem("phone", phone);
+    localStorage.setItem("message", message);
+
+    if (email == "" || firstname == "" || lastname == "" || phone == "" || message == "") {
+        alert("Please fill in all fields");
+        return;
+    } else {
+        alert("Form submitted successfully!");
+    }
+}
+
+
 function learnMore(productName) {
     alert(`To get more details about ${productName}, please search on Google.`);
   }
